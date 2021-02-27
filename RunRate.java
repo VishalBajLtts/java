ef4ff77b1dab2347780b5db97235bb310e5b441e
VishalBajLtts <78848539+VishalBajLtts@users.noreply.github.com>
2021-02-27 17:02:04 +0530

    Exception Handling runrate
    Exception Handling runrate

--- RunRate.java
+++ RunRate.java
@@ -1,31 +1,27 @@
 import java.io.*;  
 public class RunRate {
-    BufferedReader scan = new BufferedReader(new InputStreamReader(System.in)); 
-  int runs,overs;
-    float runRate;
-    public void input()throws IOException{
+    
+  
+    public static void main(String[] args) throws IOException {
         try{
+            int runs,overs;
+            float runRate;
+            BufferedReader scan = new BufferedReader(new InputStreamReader(System.in)); 
+            RunRate obj=new RunRate();
             System.out.println("Enter Runs Scored: ");
-           
             runs = Integer.parseInt(scan.readLine());
             System.out.println("Enter the over faced: ");
+            overs = Integer.parseInt(scan.readLine());
+            runRate=runs/overs;
+            System.out.println("current runrate  :"+runRate);
          
            overs= Integer.parseInt(scan.readLine());
         }
-        catch(NumberFormatException e){
+        catch(Exception e){
             System.out.println("Error Code: "+e);
             System.exit(0);
         }
-    }
-
-    public void compute(){
-        runRate=runs/overs;
-        System.out.println("current runrate  :"+runRate);
-    }
-
-    public static void main(String[] args) throws IOException {
-        RunRate obj=new RunRate();
-        obj.input();
-        obj.compute();
+        
+      
     }
 }
